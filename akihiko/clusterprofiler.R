6f9497b938e18de69303e25ee2cf9316ab380572
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-10-25 22:29:12 +0800

    Update clusterprofiler.R

--- akihiko/clusterprofiler.R
+++ akihiko/clusterprofiler.R
@@ -124,11 +124,65 @@ x=compareCluster(geneEntrez, fun='enrichGO',
 pdf("dotplot.pdf",height=10,width=10)
 dotplot(x, showCategory=15, includeAll=FALSE)
 dev.off()
+##############################################################################################################################
+# pie charts
+#  H2 + MYC
+
+pdf("pie_dist_acH2AZ+MYC.pdf")
+res=read.table(pipe("more mycTFREGULOME_h2az_peaks.annStats|cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,2]))
+names(tdown) = res[,1]
+names(tdown) = paste(names(tdown)," ",round(tdown/sum(tdown)*100,digits=2),"%",sep="")
+tdown = tdown[tdown>20]
+pie(sort(tdown), main=,cex=.8)
+title("acH2az peaks overlapping Myc peaks", cex.main=.9)
+dev.off()
 
 
+pdf("enrichment_acH2AZ+MYC.pdf")
+par(mar=c(11.1,4.1,4.1,2))
+res=read.table(pipe("more mycTFREGULOME_h2az_peaks.annStats|cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,3]))
+names(tdown) = res[,1]
+tdown = tdown[as.numeric(as.character(res[,2]))>20]
+barplot(sort(tdown),las=2,ylim=c(-4,6),ylab="Log2 Enrichment over random genomic background",col="lightblue3")
+abline(h=0)
+dev.off()
+
+#  H2 - MYC
+
+pdf("pie_dist_acH2AZ-MYC.pdf")
+res=read.table(pipe("more ach2az_-_mycTFREGULOME_peaks.annStats|cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,2]))
+names(tdown) = res[,1]
+names(tdown) = paste(names(tdown)," ",round(tdown/sum(tdown)*100,digits=2),"%",sep="")
+tdown = tdown[tdown>140]
+pie(sort(tdown), main=,cex=.8)
+title("acH2az peaks not overlapping Myc peaks", cex.main=.9)
+dev.off()
 
 
-
+pdf("enrichment_acH2AZ-MYC.pdf.pdf")
+par(mar=c(11.1,4.1,4.1,2))
+res=read.table(pipe("more ach2az_-_mycTFREGULOME_peaks.annStats|cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,3]))
+names(tdown) = res[,1]
+tdown = tdown[as.numeric(as.character(res[,2]))>140]
+barplot(sort(tdown),las=2,ylim=c(-4,6),ylab="Log2 Enrichment over random genomic background",col="lightblue3")
+abline(h=0)
+dev.off()
 
 
 
