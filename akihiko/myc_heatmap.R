b6f06571a2fb858bb4e1d538f646322fb2238881
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-02-11 18:20:12 +0800

    Update myc_heatmap.R

--- akihiko/myc_heatmap.R
+++ akihiko/myc_heatmap.R
@@ -1,7 +1,5 @@
 library(ComplexHeatmap)
 options(scipen=999)
-library(gplots)
-library(factoextra)
 library(RColorBrewer)
 colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(9))
 options(bitmapType="cairo")
@@ -15,9 +13,9 @@ exp <-exp[exp$Gene %in% genes,]
 exp.sig <- exp[,8:13]
 rownames(exp.sig) <- as.character(exp[,1])
 
-exp.log2 <- as.matrix(log2(exp.sig))
+exp.log2 <- as.matrix(log2(exp.sig+1))
 exp.log2 = exp.log2[apply(exp.log2,1,sd)!=0,]
 
-Heatmap(exp.log2,
+Heatmap(scale(exp.log2),
 show_row_names = TRUE,show_column_names = TRUE,name = "Expression",row_dend_reorder = TRUE, column_dend_reorder = TRUE,
 clustering_distance_columns = "pearson", clustering_distance_rows = "pearson")
