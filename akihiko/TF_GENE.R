3c62bded315ef22a1e0d8742d2a8c99314d89c72
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-02-17 19:33:12 +0800

    Update TF_GENE.R

--- akihiko/TF_GENE.R
+++ akihiko/TF_GENE.R
@@ -8,8 +8,12 @@ more pu1_GSE58128_liftover2mm10.anno|grep "promoter-TSS"|cut -f16|more|sort|uniq
 more TAL1_MEL.anno|grep "promoter-TSS"|cut -f16|more|sort|uniq > TAL1_MEL.txt
 #####################################################################################################################################
 #####################################################################################################################################
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
+
 exp <- read.csv("Tip60_RNAseq_triplicates.csv")
-#exp$Gene <- toupper(exp$Gene)
+exp$Gene <- toupper(exp$Gene)
 exp <- exp[,c(1,3)]
 
 act <- read.table("actRatio.txt",sep="\t",header=T)
@@ -17,5 +21,30 @@ act <- read.table("actRatio.txt",sep="\t",header=T)
 
 tal1<-read.table("TAL1_MEL.txt")
 pu1<-read.table("pu1_GSE58128_Targetgenes.txt")
+tal1 <- toupper(tal1[,1])
+pu1 <- toupper(pu1[,1])
 
 ix = match(act$Gene,exp$Gene)
+act.m <- act[!is.na(ix),]
+exp.m <- exp[ix[!is.na(ix)],]
+
+dataf <- data.frame(act = act.m$log2RatioChange, exp = exp.m$log2FoldChange)
+
+pdf("TAL1_targets.pdf")
+plot(dataf$act,dataf$exp,xlab=expression('Log'[2]*paste('acH2AZ/H2AZ Fold Change')),main="Tal1 targets",
+              ylab=expression('Log'[2]*paste('RNA expression Fold Change')),col=alpha("grey",.5),pch=20 )
+
+  points(dataf$act[act$Gene %in% tal1],
+       dataf$exp[act$Gene %in% tal1],
+      col="red",pch=20)
+dev.off()
+
+
+pdf("PU1_targets.pdf")
+plot(dataf$act,dataf$exp,xlab=expression('Log'[2]*paste('acH2AZ/H2AZ Fold Change')),main="PU1 targets",
+              ylab=expression('Log'[2]*paste('RNA expression Fold Change')),col=alpha("grey",.5),pch=20 )
+
+  points(dataf$act[act$Gene %in% pu1],
+       dataf$exp[act$Gene %in% pu1],
+      col="red",pch=20)
+dev.off()
